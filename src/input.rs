e85382607b202a80ae60289965194579a0da2048
Jason George <jasongeorge21@hotmail.com>
2019-11-17 10:32:29 -0800

    Input work, add crossterm as dependency

--- src/input.rs
+++ src/input.rs
@@ -18,19 +18,29 @@
 
 **************************************************************************/
 use std::io;
+use std::cell::Cell;
+extern crate crossterm;
+use crossterm::{
+    input::{input, InputEvent, KeyEvent},
+    screen::RawScreen,
+    Result,
+};
+
+pub enum InputNumericType
+{
+	Double,
+	SignedInteger,
+    UnsignedInteger
+}
 
 pub struct Input {
-    last_input_stdin: bool,
-    flushed         : bool,
-    canceled        : bool,
+    canceled    : Cell<bool>,
 }
 
 impl Default for Input {
     fn default() -> Input {
         Input {
-            last_input_stdin: false,
-            flushed         : false,
-            canceled        : false,
+            canceled    : Cell::new(false),
         }
     }
 }
@@ -38,11 +48,192 @@ impl Default for Input {
 impl Input {
     pub fn new() -> Input {
         Input { 
-            last_input_stdin: false,
-            flushed         : false,
-            canceled        : false, 
+            canceled    : Cell::new(false), 
             }
     }
-    
-    pub fn canceled(&self) -> &bool {&self.canceled}
+
+    pub fn canceled(&self) -> bool {self.canceled.get()}
+
+    pub fn get_keypress_from_console(&self) -> char
+    {
+        let mut input_value : char = '\0';
+        let mut flag_input_valid : bool = false;
+        self.canceled.set(false);
+
+        // Enable raw mode and keep the `_raw` around otherwise the raw mode will be disabled
+        let _raw = RawScreen::into_raw_mode();
+
+        // Create an input from our screen
+        let input = input();
+
+        while flag_input_valid == false
+        {
+            match input.read_char() {
+                Ok(c) => input_value = c,
+                Err(e) => println!("Read Error: {}", e),
+            }
+
+            if input_value != '\0'
+            {
+                // Set flag_input_valid to true so that loop ends on next go-around
+                flag_input_valid = true;
+            }
+            else
+            {
+                // Display a message indicating to the user the invalid input, and to try again
+                println!(" Invalid input. Please try again. Valid input is any keyboard character.");
+
+                // Sanity check: Set flag_input_valid to false to ensure that loop continues
+                flag_input_valid = false;
+            }
+        }
+
+        return input_value;
+    }
+
+    pub fn get_boolean_value_from_console(&self) -> bool {
+        let mut flag_input_valid = false;
+        let mut return_value : bool = false;
+
+        while flag_input_valid == false
+        {
+            let mut input_value : char = self.get_keypress_from_console();
+            if (input_value == 'y') || (input_value == 'Y') || (input_value == '1')
+            {
+                // Set flag_input_valid to true so that loop ends on next go-around
+                flag_input_valid = true;
+                return_value = true;
+            }
+            else if (input_value == 'n') || (input_value == 'N') || (input_value == '0')
+            {
+                // Set flag_input_valid to true so that loop ends on next go-around
+                flag_input_valid = true;
+                return_value = false;
+            }
+            else
+            {
+                // Display a message indicating to the user the invalid input, and to try again
+                println!(" Invalid input. Please try again. Valid inputs are y, Y, n, N, and 0 or 1.");
+
+                // Sanity check: Set flag_input_valid to false to ensure that loop continues
+                flag_input_valid = false;
+            }
+        }
+
+        return return_value;
+    }
+
+    pub fn get_string_from_console(&self, min_chars: u32, max_chars: u32) -> String {
+        let mut input_value : String = "".to_string();
+        let mut flag_input_valid : bool = false;
+        let input = crossterm::input::input();
+        self.canceled.set(false);
+
+        while flag_input_valid == false {
+            input_value.clear();
+
+            match input.read_line() {
+                Ok(s) => input_value = s,
+                Err(e) => println!("Read Error: {}", e),
+            }
+
+            if (((input_value.len() as u32) - 1) >= min_chars) && (((input_value.len() as u32) - 1) <= max_chars) {
+                // Set flag_input_valid to true so that loop ends on next go-around
+                flag_input_valid = true;
+            }
+            else {
+                // Display a message indicating to the user the invalid input, and to try again
+                println!(" Invalid input. Please try again. Valid input is a minimum {0}, maximum {1}-character string.", min_chars, max_chars);
+
+                // Sanity check: Set flag_input_valid to false to ensure that loop continues
+                flag_input_valid = false;
+            }
+        }
+
+        return input_value;
+    }
+
+    pub fn get_double_value_from_console(&self, mut lower_bound : f64, mut upper_bound : f64, numeric_type : InputNumericType, cancel_value: char) -> f64 {
+        let mut	input_value :f64		= 0.0;
+        let mut flag_input_valid : bool	= false;
+        let mut test_char : char		= ' ';
+        let input = crossterm::input::input();
+
+        // Reset flag
+        self.canceled.set(false);
+
+         // Ensure bounds are valid for input type
+        match numeric_type
+        {
+            InputNumericType::UnsignedInteger => {
+                if lower_bound < (std::u32::MIN as f64) {
+                    lower_bound = std::u32::MIN as f64;
+                }
+                else if upper_bound > (std::u32::MAX as f64) {
+                    upper_bound = std::u32::MAX as f64;
+                }
+            }
+            InputNumericType::SignedInteger => {
+                if lower_bound < (std::i32::MIN as f64) {
+                    lower_bound = std::i32::MIN as f64;
+                }
+                else if upper_bound > (std::i32::MAX as f64) {
+                    upper_bound = std::i32::MAX as f64;
+                }
+            }
+            InputNumericType::Double => (),
+            _ => (),
+        }
+
+        /*while (flag_input_valid == false) && (self.canceled.get() == false)
+        {
+            // Grab a test char from stdin
+            {
+                let _raw = RawScreen::into_raw_mode();  // Go into raw for just this scope
+
+                match input.read_char().iter(). {
+                    Ok(c) => input_value = c,
+                    Err(e) => println!("Read Error: {}", e),
+                }
+                std::cin >> std::ws;  // eat up any leading white spaces
+                testChar = cin.peek();
+
+                /*if ((testChar >= '0' && testChar <= '9') || (testChar == '.'))
+                {
+                    // Input is (probably) a number
+                    cin.unget();
+                }
+                else*/ if test_char == cancel_value {
+                    flag_input_valid = true;
+                    self.canceled.set(true);
+                }
+            }
+
+            if (self.canceled.get() == false) && (cin >> inputValue) && (input_value >= lower_bound) && (input_value <= upper_bound) && ((numeric_type == InputNumericType::Double) || (input_value.round() == input_value))
+            {
+                // Set flag_input_valid to true so that loop ends on next go-around
+                flag_input_valid = true;
+            }
+            else if self.canceled.get() == false {
+                // Display a message indicating to the user the invalid input, and to try again
+                match numeric_type
+                {
+                    UnsignedInteger => {
+                        println!(" Invalid input. Please try again. Valid input is an unsigned integer number ranging from {0} to {1}", lower_bound as u32, upper_bound as u32);
+                    }
+                    SignedInteger => {
+                        println!(" Invalid input. Please try again. Valid input is a signed integer number ranging from {0} to {1}", lower_bound as i32, upper_bound as i32);
+                    }
+                    _ => {
+                        println!(" Invalid input. Please try again. Valid input is a floating-point number ranging from {0} to {1}", lower_bound, upper_bound);
+                    }
+                }
+
+                // Sanity check: Set flag_input_valid to false to ensure that loop continues
+                flag_input_valid = false;
+            }
+        }*/
+
+        return input_value;
+    }
 }
